cc3515f84f1367d1c4ecf2d48ea78cf60d5d145b
ankitalpyadav <lpyadavankita@gmail.com>
2021-04-05 09:25:07 +0530

    MVN-Bank

---
+++ mvn-bank/src/test/java/com/ibm/bank/TestAccount.java
@@ -0,0 +1,46 @@
+package com.ibm.bank;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class TestAccount {
+	private Account ac;
+	
+	@BeforeEach
+	public void init() {
+		ac = new Account(100);
+	}
+	
+	@Test
+//	@Disabled
+	public void testDeposite() throws BalanceException {
+		assertEquals(200, ac.deposite(100));
+	}
+	
+	@Test
+//	@Disabled
+	public void testWithdraw() throws NumberFormatException, BalanceException {
+		assertEquals(50, ac.withdraw(50));
+	}
+	
+//	@Test
+////	@Disabled
+//	public void testNegativeDeposite() throws NumberFormatException {
+//		assertEquals(150, ac.deposite(50));
+//	}
+//	
+//	@Test
+////	@Disabled
+//	public void testNegativeWithdraw() throws NumberFormatException, BalanceException {
+//		assertEquals(50, new Account(10).withdraw(20));
+//		}
+	
+	@Test
+//	@Disabled
+	public void testInvalidWithdraw() throws NumberFormatException, BalanceException {
+		assertEquals(3, new Account(10).withdraw(7));
+	}
+}
